8fc9d7c2882dfa5d8d5e92b5351d1d71c997ccb9
mvximenko <mr.mxmnk@gmail.com>
2021-09-24 21:22:39 +0900

    Add Upload

--- src/views/Manage.vue
+++ src/views/Manage.vue
@@ -2,52 +2,7 @@
   <section class="container mx-auto mt-6">
     <div class="md:grid md:grid-cols-3 md:gap-4">
       <div class="col-span-1">
-        <div class="bg-white rounded border border-gray-200 relative flex flex-col">
-          <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
-            <span class="card-title">Upload</span>
-            <i class="fas fa-upload float-right text-green-400 text-2xl"></i>
-          </div>
-          <div class="p-6">
-            <div
-              class="
-                w-full
-                px-10
-                py-20
-                rounded
-                text-center
-                cursor-pointer
-                border border-dashed border-gray-400
-                text-gray-400
-                transition
-                duration-500
-                hover:text-white
-                hover:bg-green-400
-                hover:border-green-400 hover:border-solid
-              "
-            >
-              <h5>Drop your files here</h5>
-            </div>
-            <hr class="my-6" />
-            <div class="mb-4">
-              <div class="font-bold text-sm">Just another song.mp3</div>
-              <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-                <div class="transition-all progress-bar bg-blue-400" style="width: 75%"></div>
-              </div>
-            </div>
-            <div class="mb-4">
-              <div class="font-bold text-sm">Just another song.mp3</div>
-              <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-                <div class="transition-all progress-bar bg-blue-400" style="width: 35%"></div>
-              </div>
-            </div>
-            <div class="mb-4">
-              <div class="font-bold text-sm">Just another song.mp3</div>
-              <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-                <div class="transition-all progress-bar bg-blue-400" style="width: 55%"></div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <app-upload />
       </div>
       <div class="col-span-2">
         <div class="bg-white rounded border border-gray-200 relative flex flex-col">
@@ -167,3 +122,14 @@
     </div>
   </section>
 </template>
+
+<script>
+import AppUpload from '@/components/Upload.vue';
+
+export default {
+  name: 'Manage',
+  components: {
+    AppUpload,
+  },
+};
+</script>
