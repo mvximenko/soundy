26231f6b2947e56536c52115ac45479d7feee40b
mvximenko <mr.mxmnk@gmail.com>
2021-10-21 20:16:31 +0900

    Add query parameters

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -145,6 +145,10 @@ export default {
       return;
     }
 
+    const { sort } = this.$route.query;
+
+    this.sort = sort === '1' || sort === '2' ? sort : '1';
+
     this.song = docSnapshot.data();
     this.getComments();
   },
@@ -184,6 +188,19 @@ export default {
           docID: document.id,
           ...document.data(),
         });
+      });
+    },
+  },
+  watch: {
+    sort(newVal) {
+      if (newVal === this.$route.query.sort) {
+        return;
+      }
+
+      this.$router.push({
+        query: {
+          sort: newVal,
+        },
       });
     },
   },
