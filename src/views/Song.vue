db6afb95a5041b13260d62cc1b9bac2e0d3f98cd
mvximenko <mr.mxmnk@gmail.com>
2021-10-21 20:03:51 +0900

    Sort comments

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -61,6 +61,7 @@
           </button>
         </vee-form>
         <select
+          v-model="sort"
           class="
             block
             mt-4
@@ -83,12 +84,12 @@
   <ul class="container mx-auto">
     <li
       class="p-6 bg-gray-50 border border-gray-200"
-      v-for="comment in comments"
+      v-for="comment in sortedComments"
       :key="comment.docID"
     >
       <div class="mb-5">
         <div class="font-bold">{{ comment.name }}</div>
-        <time>{{ comment.datePosted }}</time>
+        <time>{{ comment.sortedComments }}</time>
       </div>
 
       <p>{{ comment.content }}</p>
@@ -121,10 +122,20 @@ export default {
       comment_alert_variant: 'bg-blue-500',
       comment_alert_message: 'Please wait! Your comment is being submitted',
       comments: [],
+      sort: '1',
     };
   },
   computed: {
     ...mapState(['userLoggedIn']),
+    sortedComments() {
+      return this.comments.slice().sort((a, b) => {
+        if (this.sort === '1') {
+          return new Date(b.datePosted) - new Date(a.datePosted);
+        }
+
+        return new Date(a.datePosted) - new Date(b.datePosted);
+      });
+    },
   },
   async created() {
     const docSnapshot = await getDoc(doc(songsCollection, this.$route.params.id));
@@ -153,6 +164,8 @@ export default {
       };
 
       await addDoc(commentsCollection, comment);
+
+      this.getComments();
 
       this.comment_in_submission = false;
       this.comment_alert_variant = 'bg-green-500';
