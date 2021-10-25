1b44e6c4d531c002b57e279b3248a0e88ebc5e12
mvximenko <mr.mxmnk@gmail.com>
2021-10-25 21:14:42 +0900

    Update SongItem

--- src/components/SongItem.vue
+++ src/components/SongItem.vue
@@ -22,10 +22,16 @@
     </div>
 
     <div class="text-gray-600 text-lg">
-      <span class="comments">
-        <i class="fa fa-comments text-gray-600"></i>
-        {{ song.comment_count }}
-      </span>
+      <router-link
+        custom
+        :to="{ name: 'Song', params: { id: song.docID }, hash: '#comments' }"
+        v-slot="{ navigate }"
+      >
+        <span class="comments" @click="navigate">
+          <i class="fa fa-comments text-gray-600"></i>
+          {{ song.comment_count }}
+        </span>
+      </router-link>
     </div>
   </li>
 </template>
--- src/views/Song.vue
+++ src/views/Song.vue
@@ -19,7 +19,7 @@
     </div>
   </section>
 
-  <section class="container mx-auto mt-6">
+  <section class="container mx-auto mt-6" id="comments">
     <div class="bg-white rounded border border-gray-200 relative flex flex-col">
       <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
         <span class="card-title">Comments ({{ song.comment_count }})</span>
