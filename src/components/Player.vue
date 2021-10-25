7851054f2d3abb0d5ef359554a174bdf31209794
mvximenko <mr.mxmnk@gmail.com>
2021-10-25 21:13:43 +0900

    Change audio position

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -22,7 +22,10 @@
           <span class="song-title">{{ currentSong.modified_name }}</span>
           <span class="song-artist"> (Uploaded by {{ currentSong.display_name }}) </span>
         </div>
-        <span class="block w-full h-2 rounded m-1 mt-2 bg-gray-200 relative cursor-pointer">
+        <span
+          class="block w-full h-2 rounded m-1 mt-2 bg-gray-200 relative cursor-pointer"
+          @click.prevent="updateSeek"
+        >
           <span class="absolute top-neg-8 text-gray-800 text-lg" :style="{ left: playerProgress }">
             <i class="fas fa-circle"></i>
           </span>
@@ -49,7 +52,7 @@ export default {
     ...mapState(['seek', 'duration', 'playerProgress', 'currentSong']),
   },
   methods: {
-    ...mapActions(['toggleAudio']),
+    ...mapActions(['toggleAudio', 'updateSeek']),
   },
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -122,5 +122,21 @@ export default createStore({
         });
       }
     },
+    updateSeek({ state, dispatch }, payload) {
+      if (!state.sound.playing) {
+        return;
+      }
+
+      const { x, width } = payload.currentTarget.getBoundingClientRect();
+      const clickX = payload.clientX - x;
+      const percentage = clickX / width;
+      const seconds = state.sound.duration() * percentage;
+
+      state.sound.seek(seconds);
+
+      state.sound.once('seek', () => {
+        dispatch('progress');
+      });
+    },
   },
 });
